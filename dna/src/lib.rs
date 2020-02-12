4102db34a5a023a2ac3ab34751bbbc0fbb0c78a2
jlikhuva <jlikhuva@stanford.edu>
2020-02-11 23:02:41 -0500

    the central dogma

--- dna/src/lib.rs
+++ dna/src/lib.rs
@@ -95,12 +95,36 @@ pub enum AminoAcid {
 
     // Neutral Polar, amino acids
     Serine, Threonine, Tyrosine, Asparagine, Glutamine,
-    Cysteine
+    Cysteine,
+
+    STOP
 }
 
 impl fmt::Display for AminoAcid {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> result::Result<(), fmt::Error> { 
-        unimplemented!()
+        match self {
+            AminoAcid::Glycine => write!(f, "Gly"),
+            AminoAcid::Alanine => write!(f, "Ala"),
+            AminoAcid::Valine => write!(f, "Val"),
+            AminoAcid::Isoleucine => write!(f, "IIe"),
+            AminoAcid::Tryptophan => write!(f, "Try"),
+            AminoAcid::Proline => write!(f, "Pro"),
+            AminoAcid::Methionine => write!(f, "Met"),
+            AminoAcid::Leucine => write!(f, "Leu"),
+            AminoAcid::PhenylAlanine => write!(f, "Phe"),
+            AminoAcid::Asparagine => write!(f, "Asn"),
+            AminoAcid::AsparticAcid => write!(f, "Asp"),
+            AminoAcid::GlutamicAcid => write!(f, "Glu"),
+            AminoAcid::Lysine => write!(f, "Lys"),
+            AminoAcid::Arginine => write!(f, "Arg"),
+            AminoAcid::Serine => write!(f, "Ser"),
+            AminoAcid::Histidine => write!(f, "His"),
+            AminoAcid::Threonine => write!(f, "Thr"),
+            AminoAcid::Tyrosine => write!(f, "Tyr"),
+            AminoAcid::Glutamine => write!(f, "Glu"),
+            AminoAcid::Cysteine => write!(f, "Cys"),
+            AminoAcid::STOP => write!(f, "#")
+        }
     }
 }
 
@@ -153,6 +177,12 @@ impl  fmt::Display for RNASequence {
     }
 }
 
+impl fmt::Display for Codon {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> result::Result<(), fmt::Error> {
+        write!(f, "({}, {}, {})", self.codon[0], self.codon[1], self.codon[2])
+    }
+}
+
 impl DNASequence {
     pub fn new(s: String) -> Self {
         DNASequence {
@@ -201,7 +231,39 @@ impl Codon {
     }
 
     pub fn get_amino_acid(&self) -> AminoAcid {
-        unimplemented!()
+        use RNANucleotide::*;
+        use AminoAcid::*;
+        match self.codon {
+            [Uracil, Guanine, Guanine] => Tryptophan,
+            [Uracil, Uracil, Uracil] | [Uracil, Uracil, Cytosine] => PhenylAlanine,
+            [Uracil, Uracil, Adenine] | [Uracil, Uracil, Guanine] => Leucine,
+            [Adenine, Uracil, Uracil] | [Adenine, Uracil, Cytosine] => Isoleucine,
+            [Adenine, Uracil, Adenine] | [Adenine, Uracil, Guanine] => Methionine,
+            [Adenine, Adenine, Uracil] | [Adenine, Adenine, Cytosine] => Asparagine,
+            [Adenine, Adenine, Adenine] | [Adenine, Adenine, Guanine] => Lysine,
+            [Adenine, Guanine, Uracil] | [Adenine, Guanine, Cytosine] => Serine,
+            [Adenine, Guanine, Adenine] | [Adenine, Guanine, Guanine] => Arginine,
+            [Cytosine, Adenine, Uracil] | [Cytosine, Adenine, Cytosine] => Histidine,
+            [Cytosine, Adenine, Adenine] | [Cytosine, Adenine, Guanine] => Glycine,
+            [Guanine, Adenine, Uracil] | [Guanine, Adenine, Cytosine] => AsparticAcid,
+            [Uracil, Adenine, Uracil] | [Uracil, Adenine, Cytosine] => Tyrosine,
+            [Uracil, Guanine, Uracil] | [Uracil, Guanine, Cytosine] => Cysteine,
+            [Uracil, Adenine, Adenine] |
+            [Uracil, Adenine, Guanine] | [Uracil, Guanine, Adenine] => STOP,
+            [Uracil, Cytosine, Uracil] | [Uracil, Cytosine, Cytosine] |
+            [Uracil, Cytosine, Adenine] | [Uracil, Cytosine, Guanine] => Serine,
+            [Cytosine, Uracil, Uracil] | [Cytosine, Uracil, Cytosine] |
+            [Cytosine, Uracil, Adenine] | [Cytosine, Uracil, Guanine] => Leucine,
+            [Cytosine, Cytosine, Uracil] | [Cytosine, Cytosine, Cytosine] |
+            [Cytosine, Cytosine, Adenine] | [Cytosine, Cytosine, Guanine] => Proline,
+            [Adenine, Cytosine, Uracil] | [Adenine, Cytosine, Cytosine] |
+            [Adenine, Cytosine, Adenine] | [Adenine, Cytosine, Guanine] => Threonine,
+            [Guanine, Uracil, Uracil] | [Guanine, Uracil, Cytosine] |
+            [Guanine, Uracil, Adenine] | [Guanine, Uracil, Guanine]  => Valine,
+            [Guanine, Cytosine, Uracil] | [Guanine, Cytosine, Cytosine] |
+            [Guanine, Cytosine, Adenine] | [Guanine, Cytosine, Guanine] => Alanine,
+            _ => panic!(format!("{}: Not a valid Codon.", self))
+        }
     }
 }
 
